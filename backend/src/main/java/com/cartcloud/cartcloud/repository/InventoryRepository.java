9081f8532685e41e3c8aeee20bbc98e729104b7d
Greta Restelica <gretarestelica04@gmail.com>
2025-12-18 00:26:55 +0100

    Minor fixes

--- backend/src/main/java/com/cartcloud/cartcloud/repository/InventoryRepository.java
+++ backend/src/main/java/com/cartcloud/cartcloud/repository/InventoryRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     Optional<Inventory> findByProduct(Product product);
-    boolean existingByProduct(Product product);
+    boolean existsByProduct(Product product);
 }
